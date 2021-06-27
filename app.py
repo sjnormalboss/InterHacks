6a0c9439f15c2c8b7b33ee9805b5631da99122b0
Angela Tsai <chojungtsai1214@gmail.com>
2021-06-26 23:53:04 -0700

    up

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ log.disabled = True
 
 
 @app.route("/", methods=["GET", "POST"])
-def homepage():
+def trouble():
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
@@ -16,6 +16,26 @@ def homepage():
         message = request.form.get("message")
         print(
             f"\n===============\nNEW INQUIRY!\n\nNAME: {name}\nEMAIL: {email}\nSUBJECT: {subject}\nINQUIRY:\n{message}\n===============\n")
-        return render_template("troubleshoot.html")
+        return render_template("sent.html")
     else:
         return render_template("troubleshoot.html")
+
+
+@app.route("/")
+def indexpage():
+    return render_template("index.html")
+
+
+@app.route("/")
+def commandpage():
+    return render_template("command.html")
+
+
+@app.route("/")
+def faqpage():
+    return render_template("faq.html")
+
+
+@app.route("/")
+def teampage():
+    return render_template("team.html")
